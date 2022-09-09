10544abe02440dfc598c14ea1964e313dca027da
Evaldo Martins <evaldms@ufpa.br>
2022-09-09 09:03:35 -0300

    Criando um arquivo R

---
+++ exmplo3.1.R
@@ -0,0 +1,7 @@
+# Este é apenas um pequeno arquivo de script para carregar para github na internet
+# Trata-se do exemplo 3.1 de criação de peojeto e integrando ao github
+# 3.1.Método 1: Clonando um reposiório do github
+
+x <- -10:10
+y <- x^3
+plot(x, y, col = "red", type = "o", bty = "l")
