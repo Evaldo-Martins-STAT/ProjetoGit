720ff0475061301a1a477d54b444577b00c32d9a
Evaldo Martins <evaldms@ufpa.br>
2022-09-10 10:21:24 -0300

    Inclui um gráfico com o ggplot

--- exemplo3.1.R
+++ exemplo3.1.R
@@ -4,8 +4,14 @@
 
 x <- -10:10
 y <- x^3
-plot(x, y, col = "red", type = "o", bty = "l")
 z <- x^2
+
+plot(x, y, col = "red", type = "o", bty = "l")
 plot(x, z, col = "blue", type = "o", bty = "l")
 
-a=12
+
+library(ggplot2)
+df <- data.frame(x,y)
+
+ggplot(df, aes(x, y))+
+	geom_point()
